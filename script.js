1a816f8b5404e19b141d4d1e8ef3792cc778f014
truptimahajan1987 <123890990+truptimahajan1987@users.noreply.github.com>
2023-04-16 22:01:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const magnifierE1 = document.querySelector(".magnifier");
+const searchbarContainerE1 = document.querySelector(".search-bar-container");
+
+magnifierE1.addEventListener("click", () =>{
+searchbarContainerE1.classList.toggle("active");
+})
